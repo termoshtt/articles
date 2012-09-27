66d54df827a51b71d77659b826c9ce48494c07d9
TAKAGI Kentaro <kentaro0910+git@gmail.com>
2012-09-27 17:41:19 +0900

    Support search with case insensibility.

--- cgi-bin/js/search.js
+++ cgi-bin/js/search.js
@@ -2,6 +2,9 @@
 function search(){
     $(".ArticleDiv").show()
     var keys = $("#SearchForm [name=SearchKeyWard]").val().split(" ");
+    jQuery.expr[':'].contains = function(a, i, m) {
+        return jQuery(a).text().toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
+    };
     jQuery.each(keys,function(){
         $(".ArticleDiv:not(:contains("+this+"))").hide()
     })
