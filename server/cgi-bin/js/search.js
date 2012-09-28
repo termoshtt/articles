d62a8f63b48551ff1e0f4b81fad5fdd246b5adc9
Toshiki Teramura <toshiki.teramura@gmail.com>
2012-09-28 21:17:42 +0900

    move up server script to root.

--- server/cgi-bin/js/search.js
+++
@@ -1,8 +0,0 @@
-
-function search(){
-    $(".ArticleDiv").show()
-    var keys = $("#SearchForm [name=SearchKeyWard]").val().split(" ");
-    jQuery.each(keys,function(){
-        $(".ArticleDiv:not(:contains("+this+"))").hide()
-    })
-}
