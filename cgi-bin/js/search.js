850d66ce6718e8e750b9c8ef94c0f745e1b33e17
TAKAGI Kentaro <kentaro0910+git@gmail.com>
2012-10-01 22:53:02 +0900

    Rename contains to insenseContains.

--- cgi-bin/js/search.js
+++ cgi-bin/js/search.js
@@ -2,10 +2,10 @@
 function search(){
     $(".ArticleDiv").show()
     var keys = $("#SearchForm [name=SearchKeyWard]").val().split(" ");
-    jQuery.expr[':'].contains = function(a, i, m) {
+    jQuery.expr[':'].insenseContains = function(a, i, m) {
         return jQuery(a).text().toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
     };
     jQuery.each(keys,function(){
-        $(".ArticleDiv:not(:contains("+this+"))").hide()
+        $(".ArticleDiv:not(:insenseContains("+this+"))").hide()
     })
 }
