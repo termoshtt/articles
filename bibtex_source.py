8afc12ad83100a31f6e2c0d23abdb3d53dc5e5fb
Toshiki Teramura <toshiki.teramura@gmail.com>
2013-01-03 22:24:51 +0900

    enable comma setting

--- bibtex_source.py
+++ bibtex_source.py
@@ -5,6 +5,21 @@ import os
 from articles import bib2html,configure
 from optparse import OptionParser
 
+def output(entries,comma=u', '):
+    for entry in entries:
+        bibkey  = entry[u"key"]
+        title   = entry[u"title"]
+        journal = entry[u"journal"]
+        year    = entry[u"year"]
+        author  = entry[u"author"]
+        tags    = entry[u"tags"]
+
+        description = author+comma+journal+comma+year+comma+title
+        if tags:
+            description = description + comma + " ".join(tags)
+        output = bibkey + comma + description
+        print(output.encode("utf-8"))
+
 def main():
     opt_parser = OptionParser()
     opt_parser.add_option("-c","--config",action="store",type="string",
@@ -17,19 +32,7 @@ def main():
     config = configure.read(config_file)
 
     entries = bib2html.read_bibtex(config)
-    for entry in entries:
-        bibkey  = entry[u"key"]
-        title   = entry[u"title"]
-        journal = entry[u"journal"]
-        year    = entry[u"year"]
-        author  = entry[u"author"]
-        tags    = entry[u"tags"]
-
-        description = author+u"|"+journal+u"|"+year+u"|"+title
-        if tags:
-            description = description + u"|" + ",".join(tags)
-        output = bibkey + u"|" + description
-        print(output.encode("utf-8"))
+    output(entries)
 
 if __name__ == "__main__":
     main()
