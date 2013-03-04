ee695b6b9682e5bb9cb31b1d4a3c071ee0a7dc23
TAKAGI Kentaro <kentaro0910+git@gmail.com>
2013-03-04 21:35:54 +0900

    Add bibManager.py

---
+++ articles/bibManager.py
@@ -0,0 +1,44 @@
+def add_bibfile(bibfile, bibstring):
+    """
+    Add bibtex file format string(BIBSTRING) to BIBFILE.
+    """
+    bibdb = read_bibdb(bibfile)
+    bibdb = add_bib2db(bibdb, bibstring)
+    write_bibdb(bibdb, bibfile)
+
+
+def add_bib2db(bibdb, bibstring, noerror=True):
+    """
+    Add bibstring (bibtex file format string) to bibtex database.
+    If NOERROR is False, throw a exception for capturing duplicate bibtexkey.
+    """
+    from pybtex.database.input import bibtex
+    parser = bibtex.Parser()
+    from io import StringIO
+    addbib = parser.parse_stream(StringIO(bibstring))
+
+    if not noerror:
+        for key in addbib.entries:
+            if key in bibdb.entries.keys():
+                raise Warning("This key is already used.")
+
+    for key in addbib.entries:
+        bibdb.add_entry(key, addbib.entries[key])
+
+    return bibdb
+
+
+def write_bibdb(bibdb, output_file):
+    """
+    BIBDB is written in OUTPUT_FILE
+    """
+
+    from pybtex.database.output import bibtex
+    writer = bibtex.Writer()
+    writer.write_file(bibdb, output_file)
+
+
+def read_bibdb(input_file):
+     from pybtex.database.input import bibtex
+     parser = bibtex.Parser()
+     return parser.parse_file(input_file)
